52445d86ac2b8b713f78c52b18d6f04c446d7866
egesua <egesu06@gmail.com>
2024-04-03 16:49:26 +0300

    bugfix and added to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 
 import './App.css';
+import Header from './components/Header';
+import Movie from './components/Movie';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <NavBar />
+      <Movie />
     </div>
   );
 }
