25c390dfe005f382945d1e1246c53ed721229a33
egesua <egesu06@gmail.com>
2024-04-03 16:48:06 +0300

    header component created

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Movie = () => {
+const Header = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const Movie = () => {
   )
 }
 
-export default Movie;
+export default Header;
